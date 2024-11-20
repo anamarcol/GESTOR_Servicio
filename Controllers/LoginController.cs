bb58556b6341af19a72129170687a196de666e18
DESKTOP-7QBV6P2\wolff <wolffangfelipe@gmail.com>
2024-11-20 18:15:57 -0500

    Creación LoginController.cs

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,27 @@
+using GESTOR_SERVICIO.class;
+using GESTOR_SERVICIO.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace Servicios_6_8.Controllers
+{
+    [EnableCors(origins: "https://localhost:44387", headers: "*", methods: "*")]
+    [RoutePrefix("api/Login")]
+    [AllowAnonymous]
+    public class LoginController : ApiController
+    {
+        [HttpPost]
+        [Route("Ingresar")]
+        public IQueryable<LoginRespuesta> Ingresar([FromBody] Login login)
+        {
+            clsLogin _login = new clsLogin();
+            _login.login = login;
+            return _login.Ingresar();
+        }
+    }
+}
