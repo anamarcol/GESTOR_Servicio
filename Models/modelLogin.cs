e72dec531b179835f21213979d2d34703ea2ecfb
DESKTOP-7QBV6P2\wolff <wolffangfelipe@gmail.com>
2024-11-20 15:46:49 -0500

    Creación del modelLogin.cs y dentro de él las clases Login y LoginRespuesta.

---
+++ Models/modelLogin.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using.Linq;
+using System.web;
+
+namespace GESTOR_SERVICIO.class
+{
+    public class Login
+    {
+        public string Usuario { get; set; }
+        public string Clave { get; set; }
+        public string PaginaSolicitud { get; set; }
+
+    }
+    
+    public class LoginRespuesta
+    {
+        public string Role { get; set; }
+        public string Token { get; set; }
+        public string MensajeError { get; set; }
+        public string PaginaInicio { get; set; }
+        public bool Autenticado { get; set; }
+    }
+}
