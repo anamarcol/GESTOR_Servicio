ba05cb8fce38734ae1dd6ef75306a62aaa9bd4c5
DESKTOP-7QBV6P2\wolff <wolffangfelipe@gmail.com>
2024-11-20 18:03:54 -0500

    Desactivación temporal de la pagína de inicio en modelLogin.cs

--- Models/modelLogin.cs
+++ Models/modelLogin.cs
@@ -18,7 +18,7 @@ namespace GESTOR_SERVICIO.class
         public string Role { get; set; }
         public string Token { get; set; }
         public string Mensaje { get; set; }
-        public string PaginaInicio { get; set; }
+        //public string PaginaInicio { get; set; }
         public bool Autenticado { get; set; }
     }
 }
