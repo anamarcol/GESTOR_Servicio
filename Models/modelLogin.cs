4734859fea18f65d28e671b32162b2b95e05b130
DESKTOP-7QBV6P2\wolff <wolffangfelipe@gmail.com>
2024-11-20 16:47:42 -0500

    Cambio de nombre de MesajeError a Mensaje en modeloLogin.cs, loginRespuesta.

--- Models/modelLogin.cs
+++ Models/modelLogin.cs
@@ -17,7 +17,7 @@ namespace GESTOR_SERVICIO.class
     {
         public string Role { get; set; }
         public string Token { get; set; }
-        public string MensajeError { get; set; }
+        public string Mensaje { get; set; }
         public string PaginaInicio { get; set; }
         public bool Autenticado { get; set; }
     }
